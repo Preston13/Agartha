9269502c74fb78e61f4507d3bf24f5ab7056135e
toppinsb96 <31316272+toppinsb96@users.noreply.github.com>
2019-01-29 16:58:02 -0600

    Start of inventory ui

---
+++ Assets/Scripts/TestControls.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestControls : MonoBehaviour {
+
+	public GameObject backgroundPanel;
+
+	// Use this for initialization
+	void Start () {
+		backgroundPanel.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Tab)) {
+			//flip whatever the active value was
+			backgroundPanel.SetActive (!backgroundPanel.activeSelf);
+
+			//if the background was just turned on
+			if (backgroundPanel.activeSelf) {
+				//run the setup for the UI
+				UIController.S.Setup();
+			}
+		}
+	}
+
+	public void Move(Button button){
+		Debug.Log (button.name);
+	}
+}
