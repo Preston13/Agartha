45000bd3e6e6014e7032fc7bbf82cfce59023b09
NoahRutledge <noah.rutledge@gmail.com>
2019-02-11 01:15:28 -0600

    Added passive abilitites

---
+++ Assets/Scripts/Player/GuardianAbilities.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardianAbilities : MonoBehaviour
+{
+    private int VIGOR_ID = 0;
+    private int RESURGENCE_ID = 1;
+    private int TENACITY_ID = 2;
+
+    public Dictionary<int, int> abilityList = new Dictionary<int, int>();
+
+    public int vigorLevel;
+    public int resLevel;
+    public int tenacityLevel;
+    public int abilityCount;
+
+    private void Update()
+    {
+        vigorLevel = abilityList[VIGOR_ID];
+        resLevel = abilityList[RESURGENCE_ID];
+        tenacityLevel = abilityList[TENACITY_ID];
+    }
+
+    public void init()
+    {
+        Debug.Log("Ability Count: " + abilityCount);
+        for(int i = 0; i < abilityCount; i++)
+        {
+            string s = "GuardianAbility" + i;
+            abilityList[i] = 0;
+
+            if (i == VIGOR_ID)
+                setVigor(PlayerPrefs.GetInt(s, 0));
+            else if (i == RESURGENCE_ID)
+                setResurgence(PlayerPrefs.GetInt(s, 0));
+            else if (i == TENACITY_ID)
+                setTenacity(PlayerPrefs.GetInt(s, 0));
+
+        }
+    }
+
+    public void setVigor(int level)
+    {
+        //undo previous level to not stack
+        Player.S.maxHealth -= (int)(Player.S.baseHealth * (0.03 * abilityList[VIGOR_ID]));
+
+        abilityList[VIGOR_ID] = level;
+        Player.S.maxHealth += (int)(Player.S.baseHealth * (0.03 * level));
+    }
+    public void addVigorLevel()
+    {
+        setVigor(abilityList[VIGOR_ID] + 1);
+    }
+
+    public void setResurgence(int level)
+    {
+        //undo previous level to not stack
+        Player.S.healthRegenRate -= 0.002 * abilityList[RESURGENCE_ID];
+
+        abilityList[RESURGENCE_ID] = level;
+        Player.S.healthRegenRate += 0.002 * level;
+    }
+    public void addResurgenceLevel()
+    {
+        setResurgence(abilityList[RESURGENCE_ID] + 1);
+    }
+
+    public void setTenacity(int level)
+    {
+        //undo previous level to not stack on top of each other
+        Player.S.tenacity -= .05 * abilityList[TENACITY_ID];
+
+        abilityList[TENACITY_ID] = level;
+        Player.S.tenacity += .05*level;
+    }
+    public void addTenacityLevel()
+    {
+        setTenacity(abilityList[TENACITY_ID] + 1);
+    }
+}
---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    static public Player S;
+
+    public int baseHealth;
+    public double currentHealth;
+    public int maxHealth;
+    public double healthRegenRate;
+    //tenacity is the resistance to status effects.  A percentage of 0-1 that reduces the time of status effects by that percent
+    public double tenacity;
+
+    public bool guarding = false;
+
+    public GuardianAbilities guardianAbilities;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        S = this;
+
+        maxHealth = baseHealth;
+        currentHealth = maxHealth;
+
+        guardianAbilities = GetComponent<GuardianAbilities>();
+        guardianAbilities.init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+            guardianAbilities.addVigorLevel();
+        if (Input.GetKeyDown(KeyCode.W))
+            guardianAbilities.addResurgenceLevel();
+        if (Input.GetKeyDown(KeyCode.E))
+            guardianAbilities.addTenacityLevel();
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            this.GetComponent<Animator>().SetBool("Guarding", !guarding);
+            guarding = !guarding;
+        }
+    }
+}
