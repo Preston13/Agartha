d1f76cc22d481bb0ed268c3323bcfd60513e3794
DESKTOP-SC6I7KB\prest <preston.grau@gmail.com>
2019-03-14 15:39:12 -0500

    Fixing the Attack

---
+++ Assets/Scripts/Enemies/EnemyStats.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStats : MonoBehaviour
+{
+    public enum Status { attacking, hit, frozen, stunned};
+    public float maxHealth;
+
+    private float curHealth;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        curHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(curHealth <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Weapon")
+        {
+            //Will pass in whatever damage the weapon does
+            TakeDamage(10);
+        }
+    }
+
+    public void TakeDamage(float damage)
+    {
+        curHealth -= damage;
+    }
+}
